249788f18af3afcc2e7e89594f73c43a9ed303d5
Inseok Yoon <diracyoon@gmail.com>
2021-11-15 19:54:55 +0900

    Fixing bugs while comparing MC & Data

---
+++ Analyzers/include/Vcb_Def.h
@@ -0,0 +1,16 @@
+#ifndef Vcb_Def_h
+#define Vcb_Def_h
+
+const float MUON_PT = 20.;
+const float MUON_ETA = 2.4;
+
+const float ELECTRON_PT = 15.;
+const float ELECTRON_ETA = 2.5;
+
+const float JET_PT = 30.;
+const float JET_ETA = 2.4;
+const float DR_LEPTON_VETO = 0.3;
+
+const float MET_PT = 20.;
+
+#endif /* Vcb_Def_h */
